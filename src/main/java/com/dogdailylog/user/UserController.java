0a91826798efb4d1fd9f031c4cce13211416420d
JasonYesBro <spdlqjqlwjs@naver.com>
2023-05-10 18:58:02 +0900

    add : signOut API close #9

--- src/main/java/com/dogdailylog/user/UserController.java
+++ src/main/java/com/dogdailylog/user/UserController.java
@@ -1,5 +1,7 @@
 package com.dogdailylog.user;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 @Controller
 public class UserController {
+	
 	/**
 	 * 회원가입 화면
 	 * @param model
@@ -21,11 +24,28 @@ public class UserController {
 		return "template/layout";
 	}
 	
+	/**
+	 * 로그인 화면
+	 * @param model
+	 * @return
+	 */
 	@GetMapping("/sign_in_view")
 	public String signInView(Model model) {
 		model.addAttribute("title", "로그인 페이지입니다.");
 		model.addAttribute("view", "user/signIn");
 		
 		return "template/layout";
+	}
+	
+	@RequestMapping("/sign_out")
+	public String signOut(HttpSession session) {
+
+		// 세션에 있는 모든 것을 비움
+		session.removeAttribute("userId");
+		session.removeAttribute("userName");
+		session.removeAttribute("userEmail");
+		
+		// 로그인 화면으로 이동
+		return "redirect:/user/sign_in_view";
 	}
 }
