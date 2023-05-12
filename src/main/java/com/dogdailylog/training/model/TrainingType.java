f68ef23f9e2ff43caff233e70cb5c1772acdffd1
JasonYesBro <spdlqjqlwjs@naver.com>
2023-05-12 19:53:31 +0900

    fix : 패키지명 변경

---
+++ src/main/java/com/dogdailylog/training/model/TrainingType.java
@@ -0,0 +1,23 @@
+package com.dogdailylog.training.model;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@NoArgsConstructor
+@Setter
+public class TrainingType {
+	private int id;
+	private int userId;
+	private int trainingType;			
+	private String trainingTitle;
+	private Date startedAt;
+	private Date finishedAt;
+	private Date createdAt;
+	private Date updatedAt;
+
+}
