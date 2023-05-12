3ee51e3ec57c59a3975ae76d662987e660d135c4
JasonYesBro <spdlqjqlwjs@naver.com>
2023-05-12 21:46:20 +0900

    fix : 메일 인증 test

--- src/test/java/com/dogdailylog/mail/bo/MailBOTest.java
+++ src/test/java/com/dogdailylog/mail/bo/MailBOTest.java
@@ -1,5 +1,6 @@
 package com.dogdailylog.mail.bo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -14,32 +15,48 @@ import com.dogdailylog.mail.model.SmtpCode;
 @SpringBootTest
 class MailBOTest {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	MailBO mailBO;
 
 	//@Test
 	void 인증코드생성() {
+		logger.info("@@@@@@@@@@@ 인증코드 생성 테스트 @@@@@@@@@@@@");
+		
 		String key = "";
 		key = mailBO.createKey();
 		
-		ObjectUtils.isEmpty(key);
+		assertNotNull(key);
 	}
 	
 	@Test
 	void 인증하기() {
+		logger.info("@@@@@@@@@@@ 인증코드 인증하기 테스트 @@@@@@@@@@@@");
+		
 		String testEmail = "tmdgus5611@gmail.com";
+		
+		SmtpCode smtpCode = new SmtpCode();
 		
 		String code = "";
 		code = mailBO.createKey(); // return boolean
+		
 		mailBO.send(testEmail);
+		
+		smtpCode.setUserEmail(testEmail);
+		smtpCode.setVerifyCode(code);
+		
 		mailBO.setSmtpCodeByEmail(testEmail);
-		SmtpCode smtpCode = null;
-		smtpCode = mailBO.getSmtpCodeByVerifyCode(code);
+		
+		SmtpCode testSmtpCode = new SmtpCode();
+		testSmtpCode = mailBO.getSmtpCodeByVerifyCode(smtpCode.getVerifyCode());
+		
 		if (ObjectUtils.isEmpty(smtpCode)) {
 			logger.info("@@@@@@@@@@ smtpCode is null @@@@@@@@@@");
 			return;
 		}
-		assertNotNull(smtpCode);
+		
+		assertThat(smtpCode.getVerifyCode()).isEqualTo(testSmtpCode.getVerifyCode());
+		
 	}
 
 }
