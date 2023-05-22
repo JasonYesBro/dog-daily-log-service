bf0242affb351366dd584f4eeffcab37b8ec8d64
JasonYesBro <spdlqjqlwjs@naver.com>
2023-05-22 21:45:18 +0900

    add : trainingBO test close #32

--- src/test/java/com/dogdailylog/training/bo/TrainingBOTest.java
+++ src/test/java/com/dogdailylog/training/bo/TrainingBOTest.java
@@ -1,6 +1,9 @@
 package com.dogdailylog.training.bo;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.text.ParseException;
@@ -16,7 +19,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.dogdailylog.training.model.TrainingType;
+import com.dogdailylog.training.dao.TrainingMapper;
+import com.dogdailylog.training.model.TrainingLog;
+import com.dogdailylog.training.model.TrainingTypeView;
 
 @SpringBootTest
 class TrainingBOTest {
@@ -24,6 +29,9 @@ class TrainingBOTest {
 	
 	@Autowired
 	TrainingBO trainingBO;
+	
+	@Autowired
+	TrainingMapper trainingMapper;
 
 	//@Test
 	void test() {
@@ -49,13 +57,88 @@ class TrainingBOTest {
 		assertThat(rowCnt).isEqualTo(1);
 	}
 	
+	
+	// @Test
+	void 훈련타입리스트() {
+		logger.info("$$$$$$$$$$$$$$ 훈련타입리스트 테스트 $$$$$$$$$$$$$$$$$");
+		
+		List<TrainingTypeView> list = new ArrayList<>();
+		list = trainingBO.getTrainingTypeViewListByUserId(6);
+		
+		assertThat(list.size()).isEqualTo(3);
+	}
+	
+	@Transactional
+	@Test
+	void 종료된훈련타입() throws ParseException {
+		logger.info("$$$$$$$$$$$$$$ 종료된훈련타입 테스트 $$$$$$$$$$$$$$$$$");
+		
+		List<TrainingTypeView> list = new ArrayList<>();
+		list = trainingBO.getTrainingTypeViewListByUserId(6);
+		
+		// cron 설정 - 설정시간 후 변경이 됐는지 테스트?
+		int cnt = trainingBO.updateOverduedTypeList();
+		assertThat(cnt).isEqualTo(3);
+	}
+	
+//	@Transactional
 //	@Test
-//	void 훈련타입리스트() {
-//		logger.info("$$$$$$$$$$$$$$ 훈련타입리스트 테스트 $$$$$$$$$$$$$$$$$");
-//		
-//		List<TrainingType> list = new ArrayList<>();
-//		list = trainingBO.getTrainingTypeListByUserId(6);
-//		
-//		assertThat(list.size()).isEqualTo(2);
-//	}
+	void 훈련로그생성() {
+		logger.info("$$$$$$$$$$$$$$ 훈련로그생성 테스트 $$$$$$$$$$$$$$$$$");
+		TrainingLog log = new TrainingLog();
+		
+		// 임시
+		int rowCnt = trainingMapper.insertTrainingLog(6, 1, "test", 1, "손을 깨물어요.", "손을 깨물지 않게 아픈척을 해봤어요.", "logImage");
+		
+		if(rowCnt > 0) {
+			log = trainingBO.getTrainingLogByLogIdAndUserId(28, 6);
+			assertNotNull(log);
+			
+		}
+	}
+	
+	//@Transactional
+	//@Test
+	void 훈련로그삭제() {
+		logger.info("$$$$$$$$$$$$$$ 훈련로그삭제 테스트 $$$$$$$$$$$$$$$$$");
+		int rowCnt = trainingBO.deleteLogByLogIdAndUserId(27, 6);
+		
+		assertEquals(rowCnt, 1);
+	}
+	
+//	@Transactional
+//	@Test
+	void 훈련일지수정하기() {
+		logger.info("$$$$$$$$$$$$$$ 훈련일지수정하기 테스트 $$$$$$$$$$$$$$$$$");
+		
+		TrainingLog beforeLog = trainingBO.getTrainingLogByLogIdAndUserId(27, 6);
+		
+		
+		trainingMapper.updateLogByLogId(beforeLog.getId(), "수정테스트", 1, "훈련원인", "수정테스트", "log/image");
+		TrainingLog afterLog = trainingBO.getTrainingLogByLogIdAndUserId(27, 6);
+		
+		logger.info("$$$$$$$$$$ 수정 전 제목 : {}, 수정 후 제목 : {}", beforeLog.getTitle(), afterLog.getTitle());
+		
+		// 같지 않으면 성공
+		assertNotSame(beforeLog, afterLog);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
