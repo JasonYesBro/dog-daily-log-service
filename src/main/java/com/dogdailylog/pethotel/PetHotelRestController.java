86dd3948cb40e35aae13a659666870f1a695cc99
JasonYesBro <spdlqjqlwjs@naver.com>
2023-05-23 21:37:58 +0900

    fix : JSONObject 타입 -> JSONArray로 수정 #50

--- src/main/java/com/dogdailylog/pethotel/PetHotelRestController.java
+++ src/main/java/com/dogdailylog/pethotel/PetHotelRestController.java
@@ -1,22 +1,31 @@
 package com.dogdailylog.pethotel;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PetHotelRestController {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/pethotel") 
 	public Map<String, Object> petHotelList() throws ParseException {
 		
 		Map<String, Object> result = new HashMap<>();
+		List<Object> listHotel = new ArrayList<>();
+		
+		
 		PetHotelAPI api = new PetHotelAPI();
 		
 		try {
@@ -24,8 +33,14 @@ public class PetHotelRestController {
 			
 			JSONParser parser = new JSONParser();
 	        JSONObject jsonObject = (JSONObject) parser.parse(list);
-
-			result.put("list", jsonObject.get("data"));
+	        
+	        JSONArray jsonArray = (JSONArray) parser.parse(jsonObject.get("data").toString());
+	        
+	        for(Object hotel : jsonArray) {
+	        	logger.info("%%%%%%%%%%%% hotel %%%%%%%%%%%%% {}", hotel);
+	        }
+			
+			result.put("list", listHotel);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
