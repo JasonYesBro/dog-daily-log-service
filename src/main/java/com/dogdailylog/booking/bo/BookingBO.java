042878cd685215f45875df92482e637b4b14c418
JasonYesBro <spdlqjqlwjs@naver.com>
2023-05-30 02:15:03 +0900

    fix : 방금 추가한 booking id 값가져오기 #56

--- src/main/java/com/dogdailylog/booking/bo/BookingBO.java
+++ src/main/java/com/dogdailylog/booking/bo/BookingBO.java
@@ -1,20 +1,55 @@
 package com.dogdailylog.booking.bo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dogdailylog.booking.dao.BookingMapper;
+import com.dogdailylog.booking.model.BookingInfo;
 
 @Service
 public class BookingBO {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private BookingMapper bookingMapper;
 	
-	public int addBooking(int userId, int schoolId, Date bookedAt) {
+	BookingInfo booking = null;
+	
+	public int addBooking(int userId, int schoolId, Date pickUpDate, String pickUpTime, int price) throws ParseException {
+//		SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm");
+//	    Date parsedDate = dateFormat.parse(pickUpTime);
 		
-		return bookingMapper.insertBooking(userId, schoolId, bookedAt);
+		Date today = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		String todayStr = sdf.format(today.getTime());
+		
+		Date bookedAt = sdf.parse(todayStr);
+	    
+		// TODO 빌더로 구현가능?
+		booking = new BookingInfo();
+		booking.setUserId(userId);
+		booking.setSchoolId(schoolId);
+		booking.setPickUpDate(pickUpDate);
+		booking.setPickUpTime(pickUpTime);
+		booking.setPrice(price);
+		booking.setBookedAt(bookedAt);
+		
+		int rowCnt = bookingMapper.insertBooking(booking);
+		// int rowCnt = bookingMapper.insertBooking(userId, schoolId, pickUpDate, pickUpTime, bookedAt, price);
+		logger.info("@@@@@@@@@@@@@@ id @@@@@@@@@@@@@@ : {}", booking.getId());
+		return rowCnt;
+	}
+	
+	public BookingInfo getBookingByUserId(int userId) {
+		logger.info("@@@@@@@@@@@@@@ id @@@@@@@@@@@@@@ : {}", booking.getId());
+		return bookingMapper.selectBookingByUserIdAndId(userId, booking.getId());
 	}
 	
 }
--- src/main/java/com/dogdailylog/booking/dao/BookingMapper.java
+++ src/main/java/com/dogdailylog/booking/dao/BookingMapper.java
@@ -1,16 +1,26 @@
 package com.dogdailylog.booking.dao;
 
-import java.util.Date;
-
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import com.dogdailylog.booking.model.BookingInfo;
 
 @Repository
 public interface BookingMapper {
 
+//	public int insertBooking(
+//			@Param("userId") int userId
+//			, @Param("schoolId") int schoolId
+//			, @Param("pickUpDate") Date pickUpDate
+//			, @Param("pickUpTime") String pickUpTime
+//			, @Param("bookedAt") Date bookedAt
+//			, @Param("price") int price);
 	public int insertBooking(
+			BookingInfo bookingInfo);
+
+	public BookingInfo selectBookingByUserIdAndId(
 			@Param("userId") int userId
-			, @Param("schoolId") int schoolId
-			, @Param("bookedAt") Date bookedAt);
+			, @Param("id") int id);
 
 }
+
